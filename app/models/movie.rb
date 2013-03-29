56c41b0c0a0f81eb01fa43697165d36cda6c2cc4
Billy Watson <billy@korrelate.com>
2013-03-28 23:24:46 -0400

    add movie for real

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,16 +1,27 @@
-class CreateMovies < ActiveRecord::Migration
-  def change
-    create_table :movies do |t|
-      t.integer :imdb_id
-      t.string :title
-      t.string :link
-      t.string :poster_link
-      t.date :release_date
-      t.string :director
-      t.string :genres
-      t.string :short_description
+require 'imdb'
 
-      t.timestamps
+class Movie < ActiveRecord::Base
+  # has_many :genres
+  attr_accessible :imdb_id, :title, :link, :poster_link, 
+    :release_date, :director, :genres, :short_description
+
+  def self.search(param)
+    ::IMDB::Search.new.movie(param).collect do |result|
+      ::IMDB::Movie.new(result.imdb_id)
     end
+  end
+
+  def self.save_from_imdb(imdb_id)
+    movie = ::IMDB::Movie.new(imdb_id)
+    self.create(
+      imdb_id: imdb_id,
+      title: movie.title,
+      link: movie.link,
+      poster_link: movie.poster,
+      release_date: movie.release_date,
+      director: movie.director,
+      genres: movie.genres.join(','),
+      short_description: movie.short_description
+    )
   end
 end
