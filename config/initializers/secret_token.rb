e9133a3a8fc90b1f0aaebae661bdd8efdaa65d39
Billy Watson <billy@korrelate.com>
2013-03-28 21:15:30 -0400

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieDatabase::Application.config.secret_token = '71197b70ea2e035d5196de19ccf7127f77439b439a8938e5b8f135c80ef0d1f23c7be4c842c88b9d9539d7ec77a611ff7a3234cd66899dd5d9f96ee7e10dbd90'
