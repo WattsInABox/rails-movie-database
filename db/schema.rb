bc1e07a4bc0fb2c3af6465183388dd5df7779214
Billy Watson <billy@korrelate.com>
2013-03-31 22:49:01 -0400

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,26 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130329023354) do
+ActiveRecord::Schema.define(:version => 20130401023825) do
+
+  create_table "genres", :force => true do |t|
+    t.integer  "movie_id"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "lists", :force => true do |t|
+    t.string   "name"
+    t.string   "color"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "movie_genres", :force => true do |t|
+    t.integer "movie_id"
+    t.integer "genre_id"
+  end
 
   create_table "movies", :force => true do |t|
     t.integer  "imdb_id"
