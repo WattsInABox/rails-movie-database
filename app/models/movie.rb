5352eb3964d45a980ba7f938ec9e2505c6741804
Billy Watson <billy@korrelate.com>
2013-03-31 22:43:43 -0400

    add genres

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,11 @@
 require 'imdb'
 
 class Movie < ActiveRecord::Base
-  # has_many :genres
   attr_accessible :imdb_id, :title, :link, :poster_link, 
     :release_date, :director, :genres, :short_description
 
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
   def self.search(param)
     ::IMDB::Search.new.movie(param).collect do |result|
       ::IMDB::Movie.new(result.imdb_id)
@@ -12,16 +13,19 @@ class Movie < ActiveRecord::Base
   end
 
   def self.save_from_imdb(imdb_id)
-    movie = ::IMDB::Movie.new(imdb_id)
-    self.create(
+    their_movie = ::IMDB::Movie.new(imdb_id)
+    our_movie = create(
       imdb_id: imdb_id,
-      title: movie.title,
-      link: movie.link,
-      poster_link: movie.poster,
-      release_date: movie.release_date,
-      director: movie.director,
-      genres: movie.genres.join(','),
-      short_description: movie.short_description
+      title: their_movie.title,
+      link: their_movie.link,
+      poster_link: their_movie.poster,
+      release_date: their_movie.release_date,
+      director: their_movie.director,
+      short_description: their_movie.short_description
     )
+
+    their_movie.genres.collect do |g| 
+      our_movie.genres.create(name: g)
+    end
   end
 end
