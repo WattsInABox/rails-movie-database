bd61b60b57bda13220f413a6230cc73f7056bbbf
Billy Watson <billy@korrelate.com>
2013-03-31 22:48:52 -0400

    use shoulda

--- test/functional/movies_controller_test.rb
+++ test/functional/movies_controller_test.rb
@@ -5,18 +5,18 @@ class MoviesControllerTest < ActionController::TestCase
     @movie = FactoryGirl.create(:movie)
   end
 
-  test "should get index" do
+  should "get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:movies)
   end
 
-  test "should get new" do
+  should "get new" do
     get :new
     assert_response :success
   end
 
-  test "should create movie" do
+  should "create movie" do
     assert_difference('Movie.count') do
       post :create, movie: FactoryGirl.attributes_for(:movie)
     end
@@ -24,22 +24,22 @@ class MoviesControllerTest < ActionController::TestCase
     assert_redirected_to movie_path(assigns(:movie))
   end
 
-  test "should show movie" do
+  should "show movie" do
     get :show, id: @movie
     assert_response :success
   end
 
-  test "should get edit" do
+  should "get edit" do
     get :edit, id: @movie
     assert_response :success
   end
 
-  test "should update movie" do
+  should "update movie" do
     put :update, id: @movie, movie: {  }
     assert_redirected_to movie_path(assigns(:movie))
   end
 
-  test "should destroy movie" do
+  should "destroy movie" do
     assert_difference('Movie.count', -1) do
       delete :destroy, id: @movie
     end
