975494e76d5c090590fba70cc60c1b4dcb9cbd7c
Billy Watson <billy@korrelate.com>
2013-05-07 23:01:40 -0400

    start on the lists page

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,7 @@ MovieDatabase::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'movies#index'
+  root :to => 'lists#index'
 
   # See how all your routes lay out with "rake routes"
 
